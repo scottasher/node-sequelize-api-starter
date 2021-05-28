fa64cce39e1c2641b0a2aff65710e0d912c37344
scott asher <scottasher@scotts-MacBook-Pro-2.local>
2021-05-27 19:17:09 -0700

    update

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USER,
+    db_pass: process.env.DB_PASS,
+    db_host: process.env.DB_HOST,
+    PORT: process.env.PORT,
+    cookieKey: process.env.COOKIEKEY,
+}
